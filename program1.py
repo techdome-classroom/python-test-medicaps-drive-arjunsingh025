a54f82120b0aad861df4d9dca0b78f0b10d45bf2
Arjun Singh Solanki <136900489+arjunsingh025@users.noreply.github.com>
2024-04-25 14:12:23 +0530

    Add files via upload

--- program1.py
+++ program1.py
@@ -1,10 +1,18 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+     def isValid(self, s: str) -> bool:
+        pair = dict(('()', '[]', '{}'))
+        st = []
+        for x in s:
+            if x in '([{':
+                st.append(x)
+            elif len(st) == 0 or x != pair[st.pop()]:
+                return False
+        return len(st) == 0
+
+s="{()}"
+sol=Solution()
+print(sol.isValid(s))
+        
     
 
 
--- program2.py
+++ program2.py
@@ -5,3 +5,31 @@ class Solution(object):
         :rtype: int
         """
         pass
+def roman_to_int(s):
+  roman_map = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+  }
+  total = 0
+  i = 0
+  while i < len(s):
+    current_value = roman_map[s[i]]
+    if i + 1 < len(s) and current_value < roman_map[s[i + 1]]:
+      # Subtraction case (special cases)
+      total += roman_map[s[i + 1]] - current_value
+      i += 2  # Skip two characters (current and next)
+    else:
+      # Addition case (normal case)
+      total += current_value
+      i += 1
+  return total
+
+# Example usage
+roman_numeral = "MCMXCIV"
+integer_value = roman_to_int(roman_numeral)
+print(f"{roman_numeral} = {integer_value}")
